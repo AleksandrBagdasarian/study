40237b14b5625aec1422f0d944704d7bf8f607eb
AleksandrBagdasarian <AleksandrBagdasarian@gmail.com>
2024-11-15 15:53:26 +0300

    len(example)//2 for median

--- strings/main.py
+++ strings/main.py
@@ -1,8 +1,6 @@
 example = 'Дезоксирибонуклеотид'
 print(example[0])
 print(example[-1])
-#Слово состоит из 20 букв, поэтому вторая половина с нечетным количеством символов либо 9 либо 11 символов
-print(example[9:20])
-print(example[11:20])
+print(example[len(example)//2:])
 print(example[::-1])
 print(example[1::2])
