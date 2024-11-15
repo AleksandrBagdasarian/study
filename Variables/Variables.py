28f7e736bc89a8d7faf1ad36ec3ecff7c6dfa5a5
AleksandrBagdasarian <AleksandrBagdasarian@gmail.com>
2024-11-15 09:21:34 +0300

    Запятые

--- Variables/Variables.py
+++ Variables/Variables.py
@@ -2,7 +2,5 @@ completed_homeworks = 12
 hours = 1.5
 course_name = 'Python'
 time_for_one_task = hours / completed_homeworks
-print('Курс:',course_name,',',                              #часть текста "Курс:" с запятой в конце
-      'всего задач:',completed_homeworks,',',               #часть текста "всего задач:" с запятой в конце
-      'затрачено часов:',hours,',',                         #часть текста "затрачено часов:" с запятой в конце
-      'среднее время выполнения',time_for_one_task,'часа.') #часть текста "среднее время выполнения:" с точкой в конце
+print('Курс:' + course_name + ', ' + 'всего задач:' + str(completed_homeworks) + ',' + 'затрачено часов: ' + str(hours)\
+      + ',' + 'среднее время выполлнения ' + str(time_for_one_task) + ' часа.')
